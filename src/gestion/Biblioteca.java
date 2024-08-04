f250599b0ae80680d7a2c65e990bc9f485843323
Jesus <jesus.ecastillo25@gmail.com>
2024-08-04 14:17:21 -0300

    Creacion de metodo agregar y eliminar libro

--- src/gestion/Biblioteca.java
+++ src/gestion/Biblioteca.java
@@ -1,5 +1,42 @@
 package gestion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
+	private List<Libro> libros;
 	
+	public Biblioteca() {
+		this.libros = new ArrayList<Libro>();
+	}
+	public boolean agregarLibro(Libro unLibro) {
+		boolean exito = false;
+		if(unLibro !=null) {
+			this.libros.add(unLibro);
+		}
+		return exito;
+	}
+	
+	public boolean eliminarLibro(String titulo) {
+		boolean exito = false;
+		int posicion = buscarIndice(titulo);
+		if(posicion !=-1) {
+			this.libros.remove(posicion);
+			exito = true;
+		}
+		return exito;
+	}
+	
+	private int buscarIndice(String titulo) {
+		int posicion =-1;
+		int i = 0;
+		while(i<this.libros.size() && posicion ==-1) {
+			if(this.libros.get(i).getTitulo().equalsIgnoreCase(titulo)) {
+				posicion = i;
+			}else {
+				i++;
+			}
+		}
+		return posicion;
+	} 
 }
