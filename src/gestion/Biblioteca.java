a0294492c430634d47c5bd61a9c8868490bdf826
Jesus <jesus.ecastillo25@gmail.com>
2024-08-04 14:28:30 -0300

    Añadiendo funcion de obtener detalle de todos los libros

--- src/gestion/Biblioteca.java
+++ src/gestion/Biblioteca.java
@@ -13,6 +13,7 @@ public class Biblioteca {
 		boolean exito = false;
 		if(unLibro !=null) {
 			this.libros.add(unLibro);
+			exito = true;
 		}
 		return exito;
 	}
@@ -30,7 +31,9 @@ public class Biblioteca {
 	private int buscarIndice(String titulo) {
 		int posicion =-1;
 		int i = 0;
+		//Buscar la posicion del libro en la lista
 		while(i<this.libros.size() && posicion ==-1) {
+			//Comparacion ignorando mayusculas
 			if(this.libros.get(i).getTitulo().equalsIgnoreCase(titulo)) {
 				posicion = i;
 			}else {
@@ -39,4 +42,16 @@ public class Biblioteca {
 		}
 		return posicion;
 	} 
+
+	public String obtenerDetallesDeLibros() {
+		String detalles = "";
+		if(this.libros.size()==0) {
+			detalles = "No hay libros";
+		}else {
+			for(Libro unLibro: this.libros) {
+				detalles = detalles +unLibro.toString()+"\n";
+			}
+		}
+		return detalles;
+	}
 }
